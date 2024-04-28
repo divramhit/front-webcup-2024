0fe3dc6e994118ee69dee1a498bf2212f11d5afd
divramhit <68056928+divramhit@users.noreply.github.com>
2024-04-28 14:27:51 +0400

    chore: added Bootstrap Icons + Dynamic Title Template + Updated README.md

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,10 @@
 import React from "react"
 import { LoginForm } from "@/components/aceternity-ui/LoginForm"
 
+export const metadata = {
+	title: 'Quack your way in!',
+}
+
 export default async function LoginPage() {
     return (
         <LoginForm />
