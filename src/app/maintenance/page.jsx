b0d7278a0360c2c96bf8282dae28283fbc115879
divramhit <68056928+divramhit@users.noreply.github.com>
2024-05-01 23:32:55 +0400

    chore: added canvas reveal card

--- src/app/maintenance/page.jsx
+++ src/app/maintenance/page.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Countdown from './countdown';
 import Image from 'next/image';
+import { Meteors } from '@/components/aceternity-ui/Meteors';
 
 const MaintenancePage = () => {
 	return (
 		<div className="maintenance-page w-screen min-h-screen dark:bg-black">
 			<div className="h-screen w-full flex flex-col p-8 lg:p-16 lg:flex-row gap-y-10 lg:gap-x-28 lg:gap-y-0">
-				<div className='border-black border-2 w-full lg:w-1/2 h-full flex flex-col gap-y-3 justify-center items-center'>
+				<div className='border-black border-2 w-full lg:w-1/2 h-full flex flex-col gap-y-3 justify-center items-center relative'>
+					<div className="meteors-wrapper absolute top-0 h-full w-full overflow-hidden">
+						<Meteors/>
+					</div>
 					<div className="logo-wrapper w-20 rounded-lg overflow-hidden aspect-square relative">
 						<Image fill src={`${process.env.NEXT_PUBLIC_APP_URL}${process.env.NEXT_PUBLIC_BASE_PATH}/images/logo.png`} />
 					</div>
@@ -18,8 +22,11 @@ const MaintenancePage = () => {
 					<video autoPlay className='h-full object-cover object-[40%_center]' loop muted src={`${process.env.NEXT_PUBLIC_APP_URL}${process.env.NEXT_PUBLIC_BASE_PATH}/videos/maintenance.mp4`}/>
 				</div>
 			</div>
-			<div className="h-screen w-full flex gap-x-28 p-16">
-				MEET OUR CODE QUACKERS
+			<div className="h-screen w-full flex gap-x-28 p-16 flex-col">
+				<span className='font-bold text-xl w-full text-center'>MEET OUR CODE QUACKERS</span>
+				<div className="code-quackers-list">
+
+				</div>
 			</div>
 		</div>
 	)
