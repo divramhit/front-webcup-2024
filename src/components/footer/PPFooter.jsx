88d1ad49e463b32fa30903903c1d50bacd3475f5
divramhit <ramhit828@gmail.com>
2024-05-05 08:40:41 +0400

    chore: WIP Footer

--- src/components/footer/PPFooter.jsx
+++ src/components/footer/PPFooter.jsx
@@ -2,11 +2,18 @@ import { ContactUsForm } from "../pp-ui/ContactUsForm";
 import PPSectionBlock from "../PPSectionBlock/PPSectionBlock";
 
 export default function PPFooter() {
+    const footerItems = [
+        {
+            title: 'About Us',
+            href: '/about-us'
+        }
+    ]
+
     return (
         <>
             <footer>
                 <PPSectionBlock className={"h-[20vh]"}>
-                    Footer
+                    
                 </PPSectionBlock> 
             </footer>   
         </>
